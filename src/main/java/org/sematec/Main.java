bcd7092339123ed58a99e28e58d77e00b99765e9
Pourya Daneshvar <pourya@daneshvar.me>
2023-11-15 20:03:04 +0330

    Stream read from a file and print its data

--- src/main/java/org/sematec/Main.java
+++ src/main/java/org/sematec/Main.java
@@ -1,25 +1,18 @@
 package org.sematec;
 
-import java.sql.Array;
-import java.util.*;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
 
-        Account firstAccount = new Account("A123004", "Hasan George", 100.000);
-        Account secondAccount = new Account("A123005", "Parham Baghi", 2300.040);
-        Account thirdAccount = new Account("A123006", "Afsaneh Samadi", 300.008);
-        Account forthAccount = new Account("A123007", "Samaneh Ostad", 300.008);
-
-
-        List<Account> account = Arrays.asList(firstAccount, secondAccount, thirdAccount, forthAccount);
-        Map<Double, List<Account>> oo = account.stream().collect(Collectors.groupingBy(p -> p.accountBalance));
-
-        oo.forEach((balance, p) -> System.out.format("%s , %s \n", balance));
+        try (Stream<String> stream = Files.lines(Paths.get("/Users/daneshvar/Downloads/file.txt"))) {
+            stream.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
