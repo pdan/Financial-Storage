14ea47d1ea5d46374026c702bae766b51fcea295
Pourya Daneshvar <pourya@daneshvar.me>
2023-11-16 11:32:47 +0330

    Write into file

--- src/main/java/org/sematec/Main.java
+++ src/main/java/org/sematec/Main.java
@@ -1,18 +1,39 @@
 package org.sematec;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.Buffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+        FileWriter fw = null;
+        boolean bool = false;
+        try  {
+            File f = new File("/Users/daneshvar/Downloads/file23.txt");
+//            bool = f.createNewFile();
 
-
-        try (Stream<String> stream = Files.lines(Paths.get("/Users/daneshvar/Downloads/file.txt"))) {
-            stream.forEach(System.out::println);
+            fw = new FileWriter(f.getPath(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.append("OkOkKoko");
+            bw.newLine();
+            bw.write(System.lineSeparator());
+            bw.flush();
+            System.out.println("File created: "+ bool);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            if(fw != null) {
+                try {
+                    fw.close();
+                } catch (IOException e) {
+
+                }
+            }
         }
     }
 }
